5b8db8ccab57098783480366502e8b46ea5f9e66
alexandermaxwellogle <maxogle@gmail.com>
2022-02-10 23:47:23 -0500

    commit tree 25281d52ae3b9c4aaaba959892ada9a2bfc72ed2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+
+const thankYouNames = ['Guadalupe', 'Ollie', 'Aki'];
+
+const occasion = ["surprise"]
+
+const cards = []
+
+function writeCards(thankYouNames, occasion) {
+    for (let i = 0; i < thankYouNames.length; i++) {
+        cards.push(`Thank you, ${thankYouNames[i]}, for the wonderful ${occasion} gift!`);
+        debugger; 
+    }
+    
+    return cards;
+}
+    
+const number = 4
+function countDown(number) { 
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+    return number 
+
+}
